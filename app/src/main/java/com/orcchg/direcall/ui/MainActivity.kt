156a52fe6a6b1c02cf262d4871b2f7cc6db4777e
Maxim Alov <Alov.M.D@sberbank.ru>
2020-10-08 09:23:03 -0700

    Fix crash while inflating already attached Fragment - start destination

--- app/src/main/java/com/orcchg/direcall/ui/MainActivity.kt
+++ app/src/main/java/com/orcchg/direcall/ui/MainActivity.kt
@@ -2,10 +2,5 @@ package com.orcchg.direcall.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import com.orcchg.direcall.R
-import com.orcchg.direcall.databinding.ActivityMainBinding
-import com.orcchg.direcall.viewBindings
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-    private val binding by viewBindings(ActivityMainBinding::inflate)
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
