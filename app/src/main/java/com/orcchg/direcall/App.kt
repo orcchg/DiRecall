31f9d5b465550e5355f451060b4d2559afa8ba66
Maxim Alov <Alov.M.D@sberbank.ru>
2020-10-08 11:45:22 -0700

    Add logging

---
+++ app/src/main/java/com/orcchg/direcall/App.kt
@@ -0,0 +1,12 @@
+package com.orcchg.direcall
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
