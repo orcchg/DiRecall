57bc46c4cebeeb46e1273934c33a09747ea372cc
Maxim Alov <Alov.M.D@sberbank.ru>
2020-10-16 04:05:26 -0700

    Bind view model args dynamically via Koin

--- app/src/main/java/com/orcchg/direcall/ui/GithubRepoFragment.kt
+++ app/src/main/java/com/orcchg/direcall/ui/GithubRepoFragment.kt
@@ -5,17 +5,20 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.orcchg.direcall.R
 import com.orcchg.direcall.adapter.GithubRepoListAdapter
+import com.orcchg.direcall.androidutil.argument
 import com.orcchg.direcall.androidutil.observe
 import com.orcchg.direcall.androidutil.viewBindings
 import com.orcchg.direcall.databinding.FragmentGithubRepoListBinding
 import com.orcchg.direcall.viewmodel.GithubRepoViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 class GithubRepoFragment : Fragment(R.layout.fragment_github_repo_list) {
 
     private val adapter = GithubRepoListAdapter()
     private val binding by viewBindings(FragmentGithubRepoListBinding::bind)
-    private val viewModel by viewModel<GithubRepoViewModel>()
+    private val login by argument<String>("login")
+    private val viewModel by viewModel<GithubRepoViewModel> { parametersOf(login) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
