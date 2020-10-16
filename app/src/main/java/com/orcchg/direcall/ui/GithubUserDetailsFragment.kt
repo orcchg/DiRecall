951aaa3c1865210be309fff358d7d89fc563d13a
Maxim Alov <Alov.M.D@sberbank.ru>
2020-10-16 04:08:04 -0700

    Add fragments extensions

--- app/src/main/java/com/orcchg/direcall/ui/GithubUserDetailsFragment.kt
+++ app/src/main/java/com/orcchg/direcall/ui/GithubUserDetailsFragment.kt
@@ -9,6 +9,7 @@ import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.jakewharton.rxbinding3.view.clicks
 import com.orcchg.direcall.R
+import com.orcchg.direcall.androidutil.argument
 import com.orcchg.direcall.androidutil.clickDebounce
 import com.orcchg.direcall.androidutil.observe
 import com.orcchg.direcall.databinding.FragmentGithubUserDetailsBinding
@@ -18,12 +19,12 @@ import com.orcchg.direcall.viewmodel.GithubUserDetailsViewModel
 class GithubUserDetailsFragment : Fragment(R.layout.fragment_github_user_details) {
 
     private val binding by viewBindings(FragmentGithubUserDetailsBinding::bind)
+    private val login by argument<String>("login")
     private val viewModel by viewModels<GithubUserDetailsViewModel>()
 
     @SuppressLint("AutoDispose", "CheckResult")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val login = arguments?.getString("login") ?: throw NullPointerException("Missing 'login' argument")
 
         binding.btnRepoList.clicks().clickDebounce()
             .subscribe {
