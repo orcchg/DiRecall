254f811f760dd175627e290a34f2190c8454092b
Maxim Alov <Alov.M.D@sberbank.ru>
2020-10-21 22:48:16 -0700

    Upgrade build tools

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.0.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    implementation("com.android.tools.build:gradle:4.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
 
     // for custom plugins
     implementation(gradleApi())
