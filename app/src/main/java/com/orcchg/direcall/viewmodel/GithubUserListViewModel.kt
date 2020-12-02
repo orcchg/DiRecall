8352cf07f9d6da3dd4d05dd26f2db02bc0894480
Leramt <vt230319@gmail.com>
2020-12-02 13:00:45 +0300

    ANDROIDSTUDY-T-6: Add github user list ViewModel

---
+++ app/src/main/java/com/orcchg/direcall/viewmodel/GithubUserListViewModel.kt
@@ -0,0 +1,22 @@
+package com.orcchg.direcall.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.orcchg.direcall.androidutil.AutoDisposeViewModel
+import com.orcchg.direcall.domain.model.GithubUser
+import com.orcchg.direcall.domain.usecase.GetGithubUsersUseCase
+import com.uber.autodispose.autoDispose
+import timber.log.Timber
+
+class GithubUserListViewModel(
+    private val getGithubUsersUseCase: GetGithubUsersUseCase
+) : AutoDisposeViewModel() {
+
+    val userList: LiveData<List<GithubUser>> by lazy(LazyThreadSafetyMode.NONE) {
+        val liveData = MutableLiveData<List<GithubUser>>()
+        getGithubUsersUseCase.source()
+            .autoDispose(this)
+            .subscribe({ liveData.value = it }, Timber::e)
+        liveData
+    }
+}
