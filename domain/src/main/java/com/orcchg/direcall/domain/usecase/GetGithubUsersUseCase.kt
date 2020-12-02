7ebce920695c748cda079fd76970f5ead3be53b7
Leramt <vt230319@gmail.com>
2020-12-02 12:08:07 +0300

    ANDROIDSTUDY-T-5: Add get github users use-case

---
+++ domain/src/main/java/com/orcchg/direcall/domain/usecase/GetGithubUsersUseCase.kt
@@ -0,0 +1,18 @@
+package com.orcchg.direcall.domain.usecase
+
+import com.orcchg.direcall.base.Params
+import com.orcchg.direcall.base.SchedulersFactory
+import com.orcchg.direcall.base.usecase.SingleUseCase
+import com.orcchg.direcall.domain.model.GithubUser
+import com.orcchg.direcall.domain.repository.GithubRepository
+import io.reactivex.Single
+
+class GetGithubUsersUseCase(
+    private val repository: GithubRepository,
+    schedulersFactory: SchedulersFactory
+) : SingleUseCase<List<GithubUser>>(schedulersFactory) {
+
+    override fun sourceImpl(params: Params): Single<List<GithubUser>> {
+        return repository.users()
+    }
+}
