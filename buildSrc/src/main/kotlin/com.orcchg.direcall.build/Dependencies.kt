ba00f109b79250bdbd808784970cc33bde7d78cf
Leramt <vt230319@gmail.com>
2021-01-18 14:27:15 +0300

    ANDROIDSTUDY-T-26: Provide dagger2 dependencies to gradle build scripts

--- buildSrc/src/main/kotlin/com.orcchg.direcall.build/Dependencies.kt
+++ buildSrc/src/main/kotlin/com.orcchg.direcall.build/Dependencies.kt
@@ -10,7 +10,7 @@ object Versions {
     const val buildToolsVersion = "30.0.0"
     const val constraintLayout = "2.0.0-rc1"
     const val coreKtx = "1.3.1"
-    const val dagger = "2.20"
+    const val dagger = "2.31"
     const val espresso = "3.2.0"
     const val fragmentKtx = "1.2.5"
     const val glide = "4.11.0"
@@ -53,8 +53,6 @@ object Dependencies {
 
     object Dagger{
         @JvmStatic val dagger = dependency("com.google.dagger:dagger:${Versions.dagger}")
-        @JvmStatic val daggerAndroid = dependency("com.google.dagger:dagger-android-support:${Versions.dagger}")
-        @JvmStatic val daggerAndroidProcessor = dependency("com.google.dagger:dagger-android-processor:${Versions.dagger}")
         @JvmStatic val daggerCompiler = dependency("com.google.dagger:dagger-compiler:${Versions.dagger}")
     }
 
