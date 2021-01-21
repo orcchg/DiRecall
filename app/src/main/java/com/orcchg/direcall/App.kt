7da58ba6c2e43883de829cec2ec8139c858bf18a
Leramt <vt230319@gmail.com>
2021-01-21 14:56:43 +0300

    ANDROIDSTUDY-T-28: Add NetworkComponent dagger @Component

--- app/src/main/java/com/orcchg/direcall/App.kt
+++ app/src/main/java/com/orcchg/direcall/App.kt
@@ -1,12 +1,18 @@
 package com.orcchg.direcall
 
 import android.app.Application
+import com.orcchg.direcall.data.di.DaggerNetworkComponent
+import com.orcchg.direcall.data.remote.GithubUserCloudRest
 import timber.log.Timber
 
 class App : Application() {
-
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
     }
+
+    val networkComponent = DaggerNetworkComponent.create()
+
+    fun provideGithubUserCloudRest(): GithubUserCloudRest =
+        networkComponent.retrofit().create(GithubUserCloudRest::class.java)
 }
--- data/src/main/java/com/orcchg/direcall/data/di/NetworkComponent.kt
+++ data/src/main/java/com/orcchg/direcall/data/di/NetworkComponent.kt
@@ -3,9 +3,7 @@ package com.orcchg.direcall.data.di
 import com.orcchg.direcall.data.remote.CloudModule
 import dagger.Component
 import retrofit2.Retrofit
-import javax.inject.Singleton
 
-@Singleton
 @Component(modules = [CloudModule::class])
 interface NetworkComponent {
     fun retrofit(): Retrofit
